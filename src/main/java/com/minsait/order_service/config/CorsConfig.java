13eb135132fcd934740239e41c5de9058dc69949
ArturoTrelles <trelles91n.n@gmail.com>
2025-08-26 18:46:30 -0600

    Actualizaciones de Swagger

---
+++ src/main/java/com/minsait/order_service/config/CorsConfig.java
@@ -0,0 +1,15 @@
+package com.minsait.order_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")               // luego puedes limitar a tu dominio
+                .allowedMethods("GET","POST","PUT","DELETE","PATCH","OPTIONS");
+    }
+}
